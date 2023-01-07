8d62702670ae0cd797070490335ff0f61c1554d3
Frank Lamantia <frank@bytestormlabs.com>
2023-01-07 07:23:56 -0500

    Removing test for deleted controller. Adding auth to application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API
   respond_to :json
+  before_action :authenticate_with_token!
   # skip_before_action :verify_authenticity_token, if: proc { |c| c.request.format == "application/json" }
   # protect_from_forgery with: :null_session, if: proc { |c| c.request.format == "application/json" }
   # skip_before_filter :authenticate_user!
