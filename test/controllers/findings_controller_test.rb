e3cf425d32dd7fba47fb45485a3b5a56b3b3b1dd
Frank Lamantia <frank@bytestormlabs.com>
2023-01-07 07:51:25 -0500

    more tests

--- test/controllers/findings_controller_test.rb
+++ test/controllers/findings_controller_test.rb
@@ -1,11 +1,15 @@
 require "test_helper"
 
 class FindingsControllerTest < ActionDispatch::IntegrationTest
-  include Devise::Test::IntegrationHelpers
-  include Warden::Test::Helpers
-
   class AuthenticatedTests < FindingsControllerTest
     test "#index" do
+      get findings_url, headers: {Authorization: "Bearer: abc-123-def-456"}
+      assert_response 200
+      assert_equal 100, JSON.parse(@response.body)["data"].count
+    end
+
+    test "#index only shows logged in users findings" do
+      skip "Not implemented yet."
       get findings_url, headers: {Authorization: "Bearer: abc-123-def-456"}
       assert_response 200
       assert_equal 100, JSON.parse(@response.body)["data"].count
