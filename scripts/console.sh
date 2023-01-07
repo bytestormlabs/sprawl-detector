348a41b195c70e339968d05ed3414ada9fd86ed1
Frank Lamantia <frank@bytestormlabs.com>
2023-01-07 13:42:36 -0500

    Updating scripts to work

--- scripts/console.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-irb -r "./scripts/load_app"
--- scripts/execute.sh
+++ scripts/execute.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
+IMAGE_NAME="sprawl-detector"
+RAILS_ENV="staging"
 EXPORTS=$(aws cloudformation list-exports)
 PRIVATE_SUBNET1=$(echo $EXPORTS | jq -r '.Exports[] | select(.Name == "Staging-PrivateSubnet1").Value')
 PRIVATE_SUBNET2=$(echo $EXPORTS | jq -r '.Exports[] | select(.Name == "Staging-PrivateSubnet2").Value')
 SG=$(echo $EXPORTS | jq -r '.Exports[] | select(.Name == "Staging-MySqlSecurityGroup").Value')
-TASK_DEFINITION=$(aws ecs list-task-definitions --family sprawl-detector | jq -r ".taskDefinitionArns | last")
+TASK_DEFINITION=$(aws ecs list-task-definitions --family $IMAGE_NAME | jq -r ".taskDefinitionArns | last")
+
+OVERRIDES=$(jq --null-input -c \
+  --arg IMAGE_NAME "$IMAGE_NAME" \
+  --arg RAILS_ENV "$RAILS_ENV" \
+  '{ "containerOverrides": [ {"name": $IMAGE_NAME, "command": ["bundle", "exec", "rake", "db:migrate", "db:seed"],"environment":[{ "name": "RAILS_ENV", "value": $RAILS_ENV },{"name":"USE_SECRETS","value":"true"}]} ]}')
 
 aws ecs run-task \
-  --cluster Staging-Cluster \
+  --cluster "Staging-Cluster" \
   --count 1 \
   --network-configuration "awsvpcConfiguration={subnets=[$PRIVATE_SUBNET1,$PRIVATE_SUBNET2],securityGroups=[$SG],assignPublicIp=DISABLED}" \
-  --overrides '{ "containerOverrides": [ {"name": "sprawl-detector", "command": ["bundle", "exec", "rake", "db:migrate"],"environment":[{ "name": "RAILS_ENV", "value": "staging"},{"name":"USE_SECRETS","value":"true"}]} ]}'\
-  --task-definition "$TASK_DEFINITION"
+  --overrides $OVERRIDES \
+  --task-definition $TASK_DEFINITION
