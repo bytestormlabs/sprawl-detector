0b6fa67b7b162699f293c054cde0439fbf641025
Frank Lamantia <frank@bytestormlabs.com>
2023-01-08 19:07:55 -0500

    Slight refactoring

--- lib/detector/secrets_manager/unused_secrets.rb
+++ lib/detector/secrets_manager/unused_secrets.rb
@@ -4,12 +4,14 @@ require "assertions"
 require "date"
 
 class UnusedSecrets
+  ISSUE_TYPE = "aws-secretsmanager-secret-obsolete"
+
   include AwsSdkOperations
   include Assertions
 
   def execute(scan, region)
-    expect region, "Paramater 'region' is required."
-    expect scan, "Paramater 'scan' is required."
+    expect region, "Parameter 'region' is required."
+    expect scan, "Parameter 'scan' is required."
 
     client = Aws::SecretsManager::Client.new(region: region, credentials: scan.credentials)
     loop_until_finished(client, :list_secrets) do |response|
@@ -20,10 +22,10 @@ class UnusedSecrets
   end
 
   def process_secret(scan, region, secret)
-    target_date = DateTime.now - 60   # TODO: Refactor this
+    target_date = DateTime.now - 60   # TODO: Refactor this into some profile/setting
     resource = scan.build_resource(region, resource_type, secret.name, secret)
 
-    resource.create_finding("aws-secretsmanager-secret-obsolete") if [
+    resource.create_finding(ISSUE_TYPE) if [
       (secret.created_date < target_date),
       (secret.last_accessed_date.nil? || secret.last_accessed_date < target_date)
     ].all?
