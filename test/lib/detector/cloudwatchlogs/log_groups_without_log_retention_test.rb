1acb1e47d6665f9192e695dcd38396a6088a5295
Frank Lamantia <frank@bytestormlabs.com>
2023-01-10 11:32:06 -0500

    Adding log groups detector

---
+++ test/lib/detector/cloudwatchlogs/log_groups_without_log_retention_test.rb
@@ -0,0 +1,29 @@
+require "detector/cloudwatchlogs/log_groups_without_log_retention"
+require "base_aws_integration_test"
+require "test_helper"
+
+class LogGroupsWithoutLogRetentionTest < ActiveSupport::TestCase
+  detector = LogGroupsWithoutLogRetention.new
+
+  test "has correct service name" do
+    assert detector.service_name == "AmazonCloudWatch"
+  end
+  test "has correct resource type" do
+    assert detector.resource_type == "AWS::Logs::LogGroup"
+  end
+  test "generates default settings" do
+    assert detector.default_settings.count == 1
+  end
+
+  class IntegrationTests < BaseAwsIntegrationTest
+    detector = LogGroupsWithoutLogRetention.new
+
+    scan = Scan.create(account: Account.first, credentials: Aws::SharedCredentials.new)
+
+    test "create finding for log group with no retention" do
+      findings = Finding.count
+      detector.execute(scan, "us-east-1")
+      assert findings < Finding.count
+    end
+  end
+end
