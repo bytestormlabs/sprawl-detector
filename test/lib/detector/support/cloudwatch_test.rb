2f058779fe55759910237f68ec1e3be3b51730ed
Frank Lamantia <frank@bytestormlabs.com>
2023-01-10 12:09:23 -0500

    Finding unused transfer servers

---
+++ test/lib/detector/support/cloudwatch_test.rb
@@ -0,0 +1,19 @@
+require "detector/support/cloudwatch"
+
+class CloudwatchTest < BaseAwsIntegrationTest
+  include Cloudwatch
+
+  test "doesn't indicate zero_activity when there arent enough datapoints" do
+    check = check("AWS/RDS", "Queries")
+      .with_dimension("DBInstanceIdentifier", "byte-storm-labs-test-database-mysql-dbinstance-a7m8ktttcki8")
+      .in("us-east-1")
+    assert check.indicates_zero_activity? == false, "There aren't enough datapoints to conclude there is zero activity"
+  end
+
+  test "correctly indicates zero activity when there are enough datapoints" do
+    check = check("AWS/RDS", "Queries")
+      .with_dimension("DBInstanceIdentifier", "byte-storm-labs-test-database-mysql-dbinstance-eke8ouwvjk1c")
+      .in("us-east-1")
+    assert check.indicates_zero_activity?
+  end
+end
---
+++ test/lib/detector/transfer/unused_transfer_servers_test.rb
@@ -0,0 +1,29 @@
+require "detector/transfer/unused_transfer_servers"
+require "base_aws_integration_test"
+
+class UnusedTransferServersTest < ActiveSupport::TestCase
+  detector = UnusedTransferServers.new
+
+  test "has correct service name" do
+    assert detector.service_name == "AWS Transfer Family"
+  end
+  test "has correct resource type" do
+    assert detector.resource_type == "AWS::Transfer::Server"
+  end
+  test "generates default settings" do
+    assert detector.default_settings.count == 1
+  end
+
+  class IntegrationTests < BaseAwsIntegrationTest
+    detector = UnusedTransferServers.new
+
+    # TODO: Refactor this so it can be shared.
+    scan = Scan.create(account: Account.first, credentials: Aws::SharedCredentials.new)
+
+    test "create findings" do
+      findings = Finding.count
+      detector.execute(scan, "us-east-1")
+      assert findings < Finding.count
+    end
+  end
+end
