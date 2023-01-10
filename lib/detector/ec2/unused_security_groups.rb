2ea9b2573922b1a113b879de1c3cce56e45c92fc
Frank Lamantia <frank@bytestormlabs.com>
2023-01-10 17:47:48 -0500

    unused security groups

---
+++ lib/detector/ec2/unused_security_groups.rb
@@ -0,0 +1,51 @@
+require "detector/support/cloudwatch"
+require "aws-sdk-ec2"
+
+class UnusedSecurityGroups
+  ISSUE_TYPE = "issuetype"
+
+  include AwsSdkOperations
+  include Assertions
+  include Cloudwatch
+
+  def execute(scan, region)
+    expect region, "parameter 'region' is required."
+    expect scan, "parameter 'scan' is required."
+
+    client = Aws::EC2::Client.new(region: region, credentials: scan.credentials)
+    security_groups = []
+    network_interfaces = []
+
+    loop_until_finished(client, :describe_network_interfaces) do |response|
+      network_interfaces << response.network_interfaces
+    end
+    loop_until_finished(client, :describe_security_groups) do |response|
+      security_groups << response.security_groups
+    end
+
+    security_groups.flatten!
+    network_interfaces.flatten.each do |network_interface|
+      network_interface.groups&.each do |group|
+        security_groups.reject! do |sg|
+          sg.group_id == group.group_id
+        end
+      end
+    end
+
+    security_groups.each do |security_group|
+      resource = scan.build_resource(region, resource_type, security_group.group_name, security_group)
+      resource.create_finding(ISSUE_TYPE)
+    end
+  end
+  def service_name
+    "AWS EC2"
+  end
+
+  def resource_type
+    "AWS::EC2::SecurityGroup"
+  end
+
+  def default_settings
+    []
+  end
+end
---
+++ test/lib/detector/ec2/unused_security_groups_test.rb
@@ -0,0 +1,27 @@
+require "detector/ec2/unused_security_groups"
+require "base_aws_integration_test"
+
+class UnusedSecurityGroupsTest < ActiveSupport::TestCase
+  detector = UnusedSecurityGroups.new
+
+ test "has correct service name" do
+   assert_equal detector.service_name, "AWS EC2"
+ end
+ test "has correct resource type" do
+   assert_equal detector.resource_type, "AWS::EC2::SecurityGroup"
+ end
+ test "generates default settings" do
+   assert_equal detector.default_settings.count, 0
+ end
+
+  class IntegrationTests < BaseAwsIntegrationTest
+    detector = UnusedSecurityGroups.new
+
+    test "create resources" do
+      scan = Scan.create(account: Account.first, credentials: Aws::SharedCredentials.new)
+      findings = Finding.count
+      detector.execute(scan, "us-east-1")
+      assert findings < Finding.count
+    end
+  end
+end
