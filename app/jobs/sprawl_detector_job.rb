49f9df864eaee38a0f86bcd039b3a2b5e8b71c69
Frank Lamantia <frank@bytestormlabs.com>
2023-01-10 14:58:40 -0500

    Removing sprawl detector

--- app/jobs/sprawl_detector_job.rb
+++ app/jobs/sprawl_detector_job.rb
@@ -1,11 +1,11 @@
-require "scan_for_findings"
+# require "scan_for_findings"
 
 class SprawlDetectorJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    ctx = ScanContext.new(account_id: ENV.fetch("AWS_ACCOUNT_ID"))
+    # ctx = ScanContext.new(account_id: ENV.fetch("AWS_ACCOUNT_ID"))
 
-    ScanForFindings.new.execute(ctx)
+    # ScanForFindings.new.execute(ctx)
   end
 end
