ea3101cbd103f23e776032dfe7a8570b9996e59a
Frank Lamantia <frank@bytestormlabs.com>
2023-01-11 13:19:43 -0500

    unused load balancers

---
+++ lib/detector/elasticloadbalancingv2/unused_load_balancers.rb
@@ -0,0 +1,65 @@
+require "detector/support/cloudwatch"
+require "aws-sdk-elasticloadbalancingv2"
+
+class UnusedLoadBalancers
+  ISSUE_TYPE = "issuetype"
+
+  include AwsSdkOperations
+  include Assertions
+  include Cloudwatch
+
+  def execute(scan, region)
+    expect region, "parameter 'region' is required."
+    expect scan, "parameter 'scan' is required."
+
+    client = Aws::ElasticLoadBalancingV2::Client.new(region: region, credentials: scan.credentials)
+    loop_until_finished(client, :describe_load_balancers) do |response|
+      response.load_balancers.each do |load_balancer|
+
+        number_of_days = 14   # TODO: Refactor this
+        type = load_balancer["type"]
+        load_balancer_name = load_balancer.load_balancer_arn.split(":").last.gsub("loadbalancer/", "")
+
+        target_date = (DateTime.now - 14)
+
+        if type == "application"
+          resource = scan.build_resource(region, "AWS::ElasticLoadBalancingV2::LoadBalancer", load_balancer.load_balancer_arn, load_balancer)
+          request_count = check("AWS/ApplicationELB", "RequestCount")
+            .in(region)
+            .in_last(number_of_days)
+            .with_dimension("LoadBalancer", load_balancer_name)
+            .with(scan.credentials)
+
+          resource.create_finding(ISSUE_TYPE) if load_balancer.created_time < target_date && request_count.indicates_zero_activity?
+        elsif type == "network"
+          resource = scan.build_resource(region, "AWS::ElasticLoadBalancingV2::LoadBalancer", load_balancer.load_balancer_arn, load_balancer)
+          active_flow_count = check("AWS/NetworkELB", "ActiveFlowCount")
+            .in(region)
+            .in_last(number_of_days)
+            .with_dimension("LoadBalancer", load_balancer_name)
+            .with(scan.credentials)
+
+          resource.create_finding(ISSUE_TYPE) if load_balancer.created_time < target_date && active_flow_count.indicates_zero_activity?
+        elsif type == "classic"
+          resource = scan.build_resource(region, "AWS::ElasticLoadBalancing::LoadBalancer", load_balancer.load_balancer_arn, load_balancer)
+          request_count_check = check("AWS/ELB", "RequestCount")
+            .in(region)
+            .in_last(number_of_days)
+            .with_dimension("LoadBalancerName", load_balancer_name)
+            .with(scan.credentials)
+
+          resource.create_finding(ISSUE_TYPE) if load_balancer.created_time < target_date && request_count_check.indicates_zero_activity?
+        end
+      end
+    end
+  end
+  def service_name
+    "Amazon Elastic Load Balancing"
+  end
+
+  def default_settings
+    [
+      Setting.create_int(ISSUE_TYPE, "number_of_days_since_last_accessed", "The number of days (since today) to check for this certificate authority issuing a certificate.", 90)
+    ]
+  end
+end
---
+++ test/lib/detector/vpc/unused_vpc_endpoints_test.rb
@@ -0,0 +1,27 @@
+require "detector/vpc/unused_vpc_endpoints"
+require "base_aws_integration_test"
+
+class UnusedVpcEndpointsTest < ActiveSupport::TestCase
+  detector = UnusedVpcEndpoints.new
+
+ test "has correct service name" do
+   assert_equal detector.service_name, "Amazon Virtual Private Cloud"
+ end
+ test "has correct resource type" do
+   assert_equal detector.resource_type, "AWS::EC2::VPCEndpoint"
+ end
+ test "generates default settings" do
+   assert_equal detector.default_settings.count, 1
+ end
+
+  # class IntegrationTests < BaseAwsIntegrationTest
+  #   detector = UnusedVpcEndpoints.new
+  #
+  #   test "create resources" do
+  #     scan = Scan.create(account: Account.first, credentials: Aws::SharedCredentials.new)
+  #     before = Finding.count
+  #     detector.execute(scan, "us-east-1")
+  #     assert_equal before + 5, Finding.count
+  #   end
+  # end
+end
