1e3cd604005343d725cdace833697c5d2143fb5c
Frank Lamantia <frank@bytestormlabs.com>
2023-01-13 07:01:41 -0500

    Adding waf test

---
+++ test/lib/detector/wafv2/unused_web_acls_test.rb
@@ -0,0 +1,27 @@
+require "detector/wafv2/unused_web_acls"
+require "base_aws_integration_test"
+
+class UnusedWebAclsTest < ActiveSupport::TestCase
+  detector = UnusedWebAcls.new
+
+ test "has correct service name" do
+   assert_equal detector.service_name, "AWS WAF"
+ end
+ test "has correct resource type" do
+   assert_equal detector.resource_type, "AWS::WAFv2::WebAcl"
+ end
+ test "generates default settings" do
+   assert_equal detector.default_settings.count, 1
+ end
+
+  # class IntegrationTests < BaseAwsIntegrationTest
+  #   detector = UnusedWebAcls.new
+  #
+  #   test "create resources" do
+  #     scan = Scan.create(account: Account.first, credentials: Aws::SharedCredentials.new)
+  #     findings = Finding.count
+  #     detector.execute(scan, "us-east-1")
+  #     assert findings < Finding.count
+  #   end
+  # end
+end
