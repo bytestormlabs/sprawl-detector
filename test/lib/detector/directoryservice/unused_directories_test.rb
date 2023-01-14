8b5fee5a183acfa74031d15fa1c7b9b5248a87d5
Frank Lamantia <frank@bytestormlabs.com>
2023-01-14 10:56:13 -0500

    standardrb fixes

--- test/lib/detector/directoryservice/unused_directories_test.rb
+++ test/lib/detector/directoryservice/unused_directories_test.rb
@@ -4,15 +4,15 @@ require "base_aws_integration_test"
 class UnusedDirectoriesTest < ActiveSupport::TestCase
   detector = UnusedDirectories.new
 
-   test "has correct service name" do
-     assert_equal detector.service_name, "AWS Directory Service"
-   end
-   test "has correct resource type" do
-     assert_equal detector.resource_type, "AWS::DirectoryService::MicrosoftAD"
-   end
-   test "generates default settings" do
-     assert_equal detector.default_settings.count, 1
-   end
+  test "has correct service name" do
+    assert_equal detector.service_name, "AWS Directory Service"
+  end
+  test "has correct resource type" do
+    assert_equal detector.resource_type, "AWS::DirectoryService::MicrosoftAD"
+  end
+  test "generates default settings" do
+    assert_equal detector.default_settings.count, 1
+  end
 
   # class IntegrationTests < BaseAwsIntegrationTest
   #   detector = UnusedDirectories.new
--- test/lib/detector/elasticloadbalancing/unused_classic_load_balancers_test.rb
+++ test/lib/detector/elasticloadbalancing/unused_classic_load_balancers_test.rb
@@ -4,15 +4,15 @@ require "base_aws_integration_test"
 class UnusedClassicLoadBalancersTest < ActiveSupport::TestCase
   detector = UnusedClassicLoadBalancers.new
 
-   test "has correct service name" do
-     assert_equal detector.service_name, "Amazon Elastic Load Balancing"
-   end
-   test "has correct resource type" do
-     assert_equal detector.resource_type, "AWS::ElasticLoadBalancing::LoadBalancer"
-   end
-   test "generates default settings" do
-     assert_equal detector.default_settings.count, 1
-   end
+  test "has correct service name" do
+    assert_equal detector.service_name, "Amazon Elastic Load Balancing"
+  end
+  test "has correct resource type" do
+    assert_equal detector.resource_type, "AWS::ElasticLoadBalancing::LoadBalancer"
+  end
+  test "generates default settings" do
+    assert_equal detector.default_settings.count, 1
+  end
 
   class IntegrationTests < BaseAwsIntegrationTest
     detector = UnusedClassicLoadBalancers.new
